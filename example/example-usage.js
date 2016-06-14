96a211b8c8d8fe204b4fb04a30acc789ce6146f3
Taka Okunishi <okunishitaka.com@gmail.com>
2016-06-14 16:19:51 +0900

    Add examples

--- example/example-usage.js
+++ example/example-usage.js
@@ -1,3 +1,35 @@
 'use strict'
 
 const sgValidator = require('sg-validator')
+
+// Define a validator with JSON-Schema
+let validator = sgValidator({
+  title: 'Example Schema',
+  type: 'object',
+  properties: {
+    firstName: {
+      type: 'string'
+    },
+    lastName: {
+      type: 'string'
+    },
+    age: {
+      description: 'Age in years',
+      type: 'integer',
+      minimum: 0
+    }
+  },
+  required: [ 'firstName', 'lastName' ]
+})
+
+// Execute validation
+let errors = validator.validate({
+  firstName: 'hoge',
+  lastName: 'fuge'
+})
+
+if (errors) {
+  throw new Error('Something is wrong!')
+} else {
+  console.log('Matches to the schema!')
+}
